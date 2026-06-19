48c439e2fbe38e06dedf4ca9b3a491e676c4ab9b
mercurymirror <romainpruvost85@gmail.com>
2026-06-19 12:49:07 +0200

    add email provider config

--- config/plugins.ts
+++ config/plugins.ts
@@ -11,6 +11,23 @@ const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin =>
       },
     },
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_FROM'),
+      },
+    },
+  },
 });
 
 export default config;
